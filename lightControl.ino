80139aa4efe334666f22618ff925beb90cb3bc90
EEБ <none@none>
2021-01-18 23:19:39 +0300

    use pwm sof-start
    --HG--
    branch : pwm

--- lightControl.ino
+++ lightControl.ino
@@ -24,8 +24,7 @@ String getHex(uint32_t in)
 
 void setup() 
 {  
-  Serial.begin(115200); 
-
+  
   for(uint8_t i=1;i<=3;++i)
   {    
     pinMode(i, OUTPUT);
@@ -34,9 +33,6 @@ void setup()
   bool onOff = true;
     
 
-  Serial.println();
-  Serial.print("connecting to ");
-  Serial.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.hostname("ESP-light");
   WiFi.begin(ssid, password);
@@ -53,78 +49,95 @@ void setup()
     onOff = !onOff;
   }
   
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address:");
-  Serial.println(WiFi.localIP());
-
+  
   // Use WiFiClientSecure class to create TLS connection
  
-  Serial.print("connecting to ");
-  Serial.println(host);
   client.setFingerprint(fingerprint);
   
 }
 
 void loop() 
 {
-  static size_t nError=0;
-  static bool onOff = false;
+  static uint8_t nError=0;  
+  static uint8_t pinStatus = 0;  
+  static bool onOff=false;  
+  static uint8_t startupCntr[2] = {0};
+  constexpr uint8_t nStartSteps = 20;
+  constexpr uint8_t pinIndex[2] = {1, 3};
   for(;;)
   {
-    Serial.println("connecting...");
-    if (!client.connect(host, httpsPort)) 
-    {
-      Serial.println("connection failed");
-      if(++nError==20)
+    if(uptime % 8 == 0)
+    {    
+      if (!client.connect(host, httpsPort)) 
       {
-         ESP.restart();
-      } 
-      break;
-    }
-
-    nError=0;    
-    
-    Serial.print("requesting URL  ");
-    Serial.println(url);
-    const auto hex= getHex(uptime);
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + host + "\r\n" +
-                 "User-Agent: ESP8266\r\n" +
-                 "Accept: *//*\r\n" +
-                 "Content-Length:144\r\n" +
-                 "Connection: close\r\n\r\n" + 
-                 token + hex +
-                    "\r\n\r\n");
-    Serial.println(hex);
-    Serial.println(uptime);
-    Serial.println("request sent");
-    while (client.connected()) 
-    {
-      String line = client.readStringUntil('\n');
-      if (line == "\r") 
-      {
-        Serial.println("headers received");
+        if(++nError==20)
+        {
+           ESP.restart();
+        } 
         break;
       }
+  
+      nError=0;    
+      const auto hex= getHex(uptime);
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" +
+                   "User-Agent: ESP8266\r\n" +
+                   "Accept: *//*\r\n" +
+                   "Content-Length:144\r\n" +
+                   "Connection: close\r\n\r\n" + 
+                   token + hex +
+                      "\r\n\r\n");
+      while (client.connected()) 
+      {
+        String line = client.readStringUntil('\n');
+        if (line == "\r") 
+        {
+          break;
+        }
+      }
+      String line = client.readStringUntil('\n');
+      const auto v=line.toInt();
+  
+      const uint8_t newPins = (v & 0b11);
+  
+      const uint8_t pinOn = (~pinStatus) &   newPins;
+      const uint8_t pinOff =  pinStatus  & (~newPins);
+  
+      for(uint8_t i=1;i<=2;++i)
+      {
+         if(pinOn & i)
+         {
+            startupCntr[i-1] = nStartSteps;      
+         }
+         if(pinOff & i)
+         {
+            startupCntr[i-1] = 0;      
+            digitalWrite(pinIndex[i-1], LOW);
+         }
+      }
+      
+      pinStatus = newPins;
+             
+      digitalWrite(2, onOff);
+  
+      onOff=!onOff;
     }
-    String line = client.readStringUntil('\n');
-    Serial.println("reply was ");
-    Serial.println("==========");
-    Serial.println(line);
-    Serial.println("==========");
-    Serial.println("closing connection");    
-    const auto v=line.toInt();
-
-    digitalWrite(3,bool( v & 0b01));    
-    digitalWrite(1,bool( v & 0b10));
-    
-    digitalWrite(2, onOff);
-
-    onOff=!onOff;
-
     break;    
   }  
-  delay(2000);
+  for(uint8_t i=0;i<2;++i)
+  {
+    if(startupCntr[i])
+    { 
+      --startupCntr[i];     
+      analogWrite(pinIndex[i],50*(nStartSteps - startupCntr[i]) );
+      if(!startupCntr[i])
+      {
+        digitalWrite(pinIndex[i-1], HIGH);
+      }
+    }
+  }
+  
+  
+  delay(200);
   ++uptime;
 }
